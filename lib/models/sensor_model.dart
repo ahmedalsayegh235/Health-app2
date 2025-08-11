c16816daba47d498781489fbb725754bd4f81c63
Ahmed Alsayegh <ahmedalsayegh40@gmail.com>
2025-08-11 21:43:22 +0300

    feat: model user/sensor

---
+++ lib/models/sensor_model.dart
@@ -0,0 +1,7 @@
+class Sensor{
+  String? uid;
+  int? heartRate;
+  double? spo2;
+
+  Sensor({this.uid, this.heartRate, this.spo2});
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,9 @@
+class User{
+  String? id;
+  String? role;
+  String? fullname;
+  String? email;
+  String? phone;
+
+  User({this.id, this.role, this.fullname, this.email, this.phone});
+}
